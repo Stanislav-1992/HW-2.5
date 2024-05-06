e00307f5b6934b03ea8fd78bb97efec301f36691
Stanislav <s.tyulkov@gmail.com>
2024-05-06 17:40:30 +0300

    initial commit

---
+++ src/main/java/pro/sky/tyulkovspringhw25/EmployeeController.java
@@ -0,0 +1,39 @@
+package pro.sky.tyulkovspringhw25;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeServiceImpl employeeService;
+
+    public EmployeeController(EmployeeServiceImpl employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/add")
+    public Employee addNewEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.add(firstName, lastName);
+    }
+
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.remove(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee (@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.find(firstName, lastName);
+    }
+    @GetMapping
+    public Collection<Employee> findAll (){
+        return employeeService.findAll();
+    }
+
+}
---
+++ src/main/java/pro/sky/tyulkovspringhw25/EmployeeServiceImpl.java
@@ -0,0 +1,50 @@
+package pro.sky.tyulkovspringhw25;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+    public final List<Employee> employees;
+
+    public EmployeeServiceImpl() {
+        this.employees = new ArrayList<>();
+    }
+
+    @Override
+    public Employee add(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if(employees.contains(employee)){
+            throw new EmployeeAlreadyAddedException();
+        }
+        employees.add(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee remove(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if(employees.contains(employee)){
+            employees.remove(employee);
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Employee find(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if(employees.contains(employee)){
+            return employee;
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Collection<Employee> findAll() {
+        return Collections.unmodifiableList(employees);
+    }
+}
